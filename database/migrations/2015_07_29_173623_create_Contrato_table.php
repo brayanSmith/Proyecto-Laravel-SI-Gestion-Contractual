953c82fc6ae60d686dc84770aa28b44f11a7435f
unknown <brayan.suarezsegura308@gmail.com>
2023-11-29 19:44:34 -0500

    Se Sube el Proyecto VFinal

---
+++ database/migrations/2015_07_29_173623_create_Contrato_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContratoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contrato', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->bigInteger('n_contrato')->unique();
+			$table->datetime('fecha_inscripcion_contrato');
+			$table->biginteger('id_autorizacion_contrato')->unsigned();
+			$table->integer('id_vigencia_contractual')->unsigned();
+			$table->integer('id_coordinacion')->unsigned();
+			$table->integer('n_documento_funcionario')->unsigned();
+			$table->datetime('fecha_inicio');
+			$table->datetime('fecha_fin');
+			$table->datetime('fecha_liquidacion');
+			$table->string('area_funcionario');
+			$table->string('cargo_funcionario');
+			$table->string('objeto_contrato');
+			$table->string('tipo_labor');
+			$table->string('modo_seleccion');
+			$table->string('calidad_psp');
+			$table->string('clase_contrato');
+			$table->bigInteger('codigo_secop');
+			$table->string('nombre_codigo_secop');
+			$table->bigInteger('valor_inicial_contrato');
+			$table->bigInteger('valor_total_contrato');
+			$table->char('recurso_contrato_convenio_interactivo', 2);
+			$table->char('ejecucion_otro_municipio', 2);
+			$table->string('estado_contrato');
+
+			$table->foreign('id_vigencia_contractual')
+				  ->references('id')
+				  ->on('vigencia_contractual')
+				  ->onDelete('cascade');
+
+			$table->foreign('id_coordinacion')
+				  ->references('id')
+				  ->on('coordinacion')
+				  ->onDelete('cascade');
+
+			$table->foreign('n_documento_funcionario')
+				  ->references('id')
+				  ->on('funcionario')
+				  ->onDelete('cascade');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contrato');
+	}
+
+}
